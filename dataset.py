2e5ef7326f9f2fc754dccd65a98023d8422b22b5
esushmitha <141324946+esushmitha@users.noreply.github.com>
2025-04-17 15:52:59 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,32 +1,42 @@
-# dataset.py
-import torch
 from torch.utils.data import Dataset
 import pandas as pd
+from transformers import T5Tokenizer
 
 class SimplificationDataset(Dataset):
-    def __init__(self, csv_path, tokenizer, max_len=50):
-        df = pd.read_csv(csv_path)
-        self.pairs = []
-        self.max_len = max_len
+    def __init__(self, csv_file, tokenizer, max_len=512):
         self.tokenizer = tokenizer
-        for _, row in df.iterrows():
-            complex_ids = tokenizer.encode(row['Normal'])[:max_len]
-            simple_ids = tokenizer.encode(row['Simple'])[:max_len]
-            # Add start (<s>) and end (</s>) tokens for target sequence
-            simple_ids = [tokenizer.token_to_id['<s>']] + simple_ids + [tokenizer.token_to_id['</s>']]
-            self.pairs.append((complex_ids, simple_ids))
-        self.pad_id = tokenizer.token_to_id['<pad>']
+        self.max_len = max_len
+        self.data = pd.read_csv(csv_file)
 
     def __len__(self):
-        return len(self.pairs)
+        return len(self.data)
 
     def __getitem__(self, idx):
-        src, tgt = self.pairs[idx]
-        # Pad source
-        src = src + [self.pad_id] * (self.max_len - len(src))
-        # Pad target: assume tgt length <= max_len + 2
-        tgt = tgt + [self.pad_id] * ((self.max_len + 2) - len(tgt))
-        # For teacher forcing, input is all but last token, output is all but first
-        tgt_input = torch.tensor(tgt[:-1])
-        tgt_output = torch.tensor(tgt[1:])
-        return torch.tensor(src), tgt_input, tgt_output
+        source_text = str(self.data.iloc[idx, 0])
+        target_text = str(self.data.iloc[idx, 1])
+
+        # Tokenizing the input and target
+        source_encoding = self.tokenizer.encode_plus(
+            source_text,
+            truncation=True,
+            padding='max_length',
+            max_length=self.max_len,
+            return_tensors="pt"
+        )
+
+        target_encoding = self.tokenizer.encode_plus(
+            target_text,
+            truncation=True,
+            padding='max_length',
+            max_length=self.max_len,
+            return_tensors="pt"
+        )
+
+        # Ensure that we're returning tensors
+        input_ids = source_encoding['input_ids'].squeeze(0)  # Convert to 1D tensor
+        target_ids = target_encoding['input_ids'].squeeze(0)
+        
+        return {
+            'input': input_ids,
+            'target': target_ids
+        }
