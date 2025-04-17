d9e06a5562fe7607d7466b90b8aecc85c29f2aea
esushmitha <141324946+esushmitha@users.noreply.github.com>
2025-04-17 15:54:30 +0800

    Update tokenizer.py

--- models/tokenizer.py
+++ models/tokenizer.py
@@ -1,29 +1,23 @@
-# models/tokenizer.py
-import re
+from transformers import T5Tokenizer
 
 class Tokenizer:
-    def __init__(self):
-        # Special tokens: <pad>, <s>, </s>, <unk>
-        self.token_to_id = {'<pad>': 0, '<s>': 1, '</s>': 2, '<unk>': 3}
-        self.id_to_token = {v: k for k, v in self.token_to_id.items()}
-
-    def build_vocab(self, texts, min_freq=1):
-        freq = {}
-        for text in texts:
-            for token in self.tokenize(text):
-                freq[token] = freq.get(token, 0) + 1
-        for token, count in freq.items():
-            if count >= min_freq and token not in self.token_to_id:
-                idx = len(self.token_to_id)
-                self.token_to_id[token] = idx
-                self.id_to_token[idx] = token
-
-    def tokenize(self, text):
-        # Split text into words and punctuation
-        return re.findall(r"\w+|[^\w\s]", text.lower())
+    def __init__(self, model_name="t5-small"):
+        self.tokenizer = T5Tokenizer.from_pretrained(model_name)
+    
+    def build_vocab(self, sentences):
+        # T5 already has a built-in vocab, so this may not be needed
+        pass
 
     def encode(self, text):
-        return [self.token_to_id.get(tok, self.token_to_id['<unk>']) for tok in self.tokenize(text)]
+        return self.tokenizer.encode(text, add_special_tokens=True)
 
-    def decode(self, ids):
-        return " ".join([self.id_to_token.get(i, '<unk>') for i in ids])
+    def decode(self, token_ids):
+        return self.tokenizer.decode(token_ids, skip_special_tokens=True)
+
+    @property
+    def pad_token_id(self):
+        return self.tokenizer.pad_token_id
+
+    @property
+    def token_to_id(self):
+        return self.tokenizer.get_vocab()
